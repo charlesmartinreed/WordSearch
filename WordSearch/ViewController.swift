d4e80f1d7a09e73092871d76c96e8ab1670447a7
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-18 14:38:28 -0600

    Add capitals.json to project. Decode json to create words and test makeGrid.

--- WordSearch/ViewController.swift
+++ WordSearch/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //quick dirty JSON
+        if let path = Bundle.main.url(forResource: "capitals", withExtension: ".json") {
+            do {
+                let contents = try Data(contentsOf: path)
+                let words = try JSONDecoder().decode([Word].self, from: contents)
+                
+                let wordSearch = WordSearch()
+                wordSearch.words = words
+                wordSearch.makeGrid()
+            } catch {
+                fatalError("could not parse JSON")
+            }
+            
+            
+        }
     }
 
 
